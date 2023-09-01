d8b04b4ab0ef8f781e6d87bd633b7e8962528fd6
jc_jimenez <jcjj.system@gmail.com>
2023-09-01 10:24:39 -0400

    Update app.js
    change json domain

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 angular.module('resume', []).controller('Controller', ['$scope', '$http', 
 function($scope, $http) {
-	$http.get('https://master.d2r5j8b15o1tgt.amplifyapp.com/resume.json')
+	$http.get('https://jcjimenez.me/resume.json')
 						.success(function(data, status, headers, config){
 							console.log(data)
 							$scope.resume = data
