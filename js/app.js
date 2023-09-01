30e338736de9d5acbea2ec9b27b6ca756e2aa58c
jc_jimenez <jcjj.system@gmail.com>
2023-09-01 09:26:29 -0400

    Update app.js
    Fix json link

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 angular.module('resume', []).controller('Controller', ['$scope', '$http', 
 function($scope, $http) {
-	$http.get('https://master.d2r5j8b15o1tgt.amplifyapp.com/resume/resume.json')
+	$http.get('https://master.d2r5j8b15o1tgt.amplifyapp.com/resume.json')
 						.success(function(data, status, headers, config){
 							console.log(data)
 							$scope.resume = data
